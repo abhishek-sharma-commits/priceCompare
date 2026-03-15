ac078af3873573f94ff6a3094bcc35e85fb33de2
abhishek-sharma-commits <standup787059.com>
2026-03-16 02:13:05 +0530

    updated cors

--- server/server.js
+++ server/server.js
@@ -7,7 +7,12 @@ const compareRoutes = require("./routes/compare");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://pricebackend-wor2.onrender.com"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 const limiter = rateLimit({
