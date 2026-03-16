2147b0cf18787889302e699345ffc38808024e6f
abhishek-sharma-commits <standup787059.com>
2026-03-16 12:55:21 +0530

    fixed vite.config error

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://pricebackend-wor2.onrender.com"],
+    origin: "http://localhost:5173", 
     credentials: true,
   }),
 );
